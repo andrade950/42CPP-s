51b7a0c4f1973e6701d5e4c79f0da5b755425bca
João Miguel Andrade <157920929+andrade950@users.noreply.github.com>
2026-04-21 14:26:38 +0100

    Update attack targets in main.cpp

--- CPP03/ex01/main.cpp
+++ CPP03/ex01/main.cpp
@@ -9,7 +9,6 @@
 /*   Updated: 2026/04/16 13:07:18 by joaomart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include "ClapTrap.hpp"
 #include "ScavTrap.hpp"
 
@@ -17,30 +16,32 @@ int main()
 {
 	{
 		std::cout << std::endl << "ClapTrap" << std::endl;
-		std::string target = "BOT2";
+		ClapTrap BOT1("BOT1");
 		ClapTrap BOT2("BOT2");
-		BOT2.attack(target);
+		BOT2.attack("BOT1");
+		BOT1.takeDamage(0);
 		BOT2.beRepaired(2);
 		BOT2.takeDamage(10);
 		BOT2.takeDamage(2);
-		BOT2.attack("BOT2");
+		BOT2.attack("BOT1");
 	}
 	{
 		std::cout << std::endl << "ScavTrap" << std::endl;
-		std::string target = "BOT2";
+		ClapTrap BOT1("BOT1");
 		ScavTrap BOT3("BOT3");
-		BOT3.attack(target);
+		BOT3.attack("BOT1");
+		BOT1.takeDamage(20);
 		BOT3.beRepaired(2);
 		BOT3.takeDamage(10);
 		BOT3.takeDamage(2);
-		BOT3.attack("BOT2");
+		BOT3.attack("BOT1");
 		BOT3.guardGate();
 	}
 	{
-		std::string target = "BOT2";
 		std::cout << std::endl << "pointerrr" << std::endl;
+		ClapTrap BOT1("BOT1");
 		ClapTrap *BOT4 = new ScavTrap("BOT4");
-		BOT4->attack(target);
+		BOT4->attack("BOT1");
 		BOT4->takeDamage(100);
 		delete BOT4;
 	}
