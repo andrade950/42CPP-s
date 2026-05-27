0e27b85cf84f3eec9e329d76f6011130d23d66df
João Miguel Martinho Andrade <joaomart@c2r5s3.42porto.com>
2026-05-27 10:22:42 +0100

    add cpp09

---
+++ CPP09/ex00/main.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joaomart <joaomart@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/18 15:08:53 by joaomart          #+#    #+#             */
+/*   Updated: 2026/05/25 15:10:35 by joaomart         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "BitcoinExchange.hpp"
+
+int main(int ac, char **av)
+{
+    if (ac != 2)
+    {
+        std::cerr << "Error: could not open file." << std::endl;
+        return (1);
+    }
+    try
+    {
+        BitcoinExchange b;
+        b.processInput(av[1]);
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return (1);
+    }
+    return (0);
+}
